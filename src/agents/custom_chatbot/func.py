af02846538e362832da7f9da137e13f9f991068b
abaoxomtieu <htbqn2003@gmail.com>
2025-05-18 21:26:05 +0700

    fix: remove loger in create_promot

--- src/agents/custom_chatbot/func.py
+++ src/agents/custom_chatbot/func.py
@@ -8,19 +8,22 @@ from src.config.llm import llm_2_0
 from src.utils.logger import logger
 import re
 from src.config.mongo import bot_crud
+
+
 class State(TypedDict):
     messages: Annotated[Sequence[AnyMessage], add_messages]
     remaining_steps: int
     prompt: Optional[str]
     name: Optional[str]
 
+
 def get_info_collection(messages):
     for idx, message in enumerate(messages):
         if isinstance(message, ToolMessage):
             break
-    info = messages[idx - 1].tool_calls[0].get("args", {}).get('info','')
-    name = messages[idx - 1].tool_calls[0].get("args", {}).get('name','')
-    return name,info
+    info = messages[idx - 1].tool_calls[0].get("args", {}).get("info", "")
+    name = messages[idx - 1].tool_calls[0].get("args", {}).get("name", "")
+    return name, info
 
 
 def create_prompt(state: State):
@@ -30,19 +33,18 @@ def create_prompt(state: State):
     res = llm_2_0.invoke(
         [
             {"role": "system", "content": create_system_prompt},
-            {"role": "human", "content": info}
+            {"role": "human", "content": info},
         ]
     )
     prompt = f"name chatbot:{name}"
     prompt = res.content
-    return {"prompt": prompt,"name":name}
+    return {"prompt": prompt, "name": name}
 
 
 async def save_prompt(state: State):
-    prompt = state.get('prompt','')
+    prompt = state.get("prompt", "")
     matches = re.findall(r"```(.*?)```", prompt, re.DOTALL)
     if matches:
         prompt = matches[0]
-    name = state.get('name')
-    await bot_crud.create({"name":name,"prompt":prompt,"tools":[]})
-    logger.info(f"save_prompt {matches[0]}")
+    name = state.get("name")
+    await bot_crud.create({"name": name, "prompt": prompt, "tools": []})
